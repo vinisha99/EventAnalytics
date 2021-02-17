07275429edc487b61ee6443b179b4b9d94dc9afa
Vinisha Nagarajan <vinisha99@gmail.com>
2021-02-17 15:14:08 -0500

    initial commit - project setup

---
+++ src/main/java/com/eventAnalytics/application/EventAnalyticsApplication.java
@@ -0,0 +1,13 @@
+package com.eventAnalytics.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventAnalyticsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventAnalyticsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eventAnalytics/application/EventAnalyticsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eventAnalytics.application;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventAnalyticsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
